2d858fb1b7050d10d33816c68849b24111986826
suchiyadav934 <suchiyadav934grd@gmail.com>
2024-09-12 15:11:41 +0530

    updated

--- s.js
+++ s.js
@@ -1,5 +1,5 @@
  let display = document.getElementById("display")
  function del(){
     var display = document.getElementById("display")
-    display.value = display.value.slice(0, -1)
+    display.value = display.value.slice(0, -1);
  }
